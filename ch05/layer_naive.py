c88e918c596c503571f6bb2b8a55355a92c63301
shogo watanabe <ne.sheep.5mouths@gmail.com>
2017-02-02 12:22:38 +0900

    コメントアウト方法を修正

--- ch05/layer_naive.py
+++ ch05/layer_naive.py
@@ -1,18 +1,20 @@
-// 乗算レイヤー
+# 乗算レイヤー
 class MulLayer:
     def __init__(self):
         self.x = None
         self.y = None
 
-    // 順伝搬
+    # 順伝搬
     def forward(self, x, y):
         self.x = x
         self.y = y
         return  x * y
 
-    // 逆伝搬 dout:微分値
+    # 逆伝搬 dout:微分値
     def backward(self, dout):
+        #  XとYをひっくり返して乗算
         dx = dout * self.y
         dy = dout * self.x
 
         return dx, dy
+
