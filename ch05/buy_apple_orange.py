eae75dbbfbabb82c6669002065d589ca35cbe1e9
渡邉祥吾 <WatanabeShogo@shogo.local>
2017-02-04 06:28:34 +0900

    乗算レイヤと加算レイヤを利用した計算を実装

---
+++ ch05/buy_apple_orange.py
@@ -0,0 +1,36 @@
+# coding: utf-8
+from layer_naive import *
+
+# リンゴ(100円)✕2+オレンジ(150円)✕3 (消費税10%)
+apple = 100
+apple_num = 2
+orange = 150
+orange_num = 3
+tax = 1.1
+
+#layer
+mul_apple_layer = MulLayer()
+mul_orange_layer = MulLayer()
+add_apple_orange_layer = AddLayer()
+mul_tax_layer = MulLayer()
+
+#forward
+apple_price = mul_apple_layer.forward(apple, apple_num)
+orange_price = mul_orange_layer.forward(orange, orange_num)
+all_price = add_apple_orange_layer.forward(apple_price, orange_price)
+price = mul_tax_layer.forward(all_price, tax)
+
+#backward
+#順伝搬の逆順に実行
+dprice = 1
+dall_price, dtax = mul_tax_layer.backward(dprice)
+dapple_price, dorange_price = add_apple_orange_layer.backword(dall_price)
+dorange, dorange_num = mul_orange_layer.backward(dorange_price)
+dapple, dapple_num = mul_apple_layer.backward(dapple_price)
+
+print("price:", int(price))
+print("dApple:", dapple)
+print("dApple_num:", int(dapple_num))
+print("dOrange:", dorange)
+print("dOrange_num:", int(dorange_num))
+print("dTax:", dtax)
