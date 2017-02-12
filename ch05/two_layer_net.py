6da853d0a26540606f79270b02ab547026e4aaa3
shogo watanabe <ne.sheep.5mouths@gmail.com>
2017-02-12 10:18:33 +0900

    TwoLayerNetクラス実装

--- ch05/two_layer_net.py
+++ ch05/two_layer_net.py
@@ -28,21 +28,55 @@ class TwoLayerNet:
     #  認識(推論)をおこなう
     # 引数 x： 画像データ
     def predict(self, x):
-        # TODO 未実装
+        for layer in self.layers.values():
+            x = layer.forward(x)
 
     # 損失関数の値を求める
     # 引数 x:画像データ、 t:正解ラベル
     def loss(self, x, t):
-        # TODO 未実装
+        y = self.predict(x)
+        return self.lastLayer.forward(y, t)
 
     # 認識精度を求める
     def accuracy(self, x, t):
-        # TODO 未実装
+        y = self.predict(x)
+        y = np.argmax(y, axis=1)
+        if t.ndim != 1: t = np.argmax(t, axis=1)
+
+        accuracy = np.sum(y == t) / float(x.shape[0])
+        return accuracy
 
     # 重みパラメータに対する勾配を数値微分によって求める
     def numerical_gradient(self, x, t):
-        # TODO 未実装
+        loss_W = lambda W: self.loss(x, t)
+
+        grads = {}
+        grads['W1'] = numerical_gradient(loss_W, self,param['W1'])
+        grads['b1'] = numerical_gradient(loss_W, self,param['b1'])
+        grads['W2'] = numerical_gradient(loss_W, self,param['W2'])
+        grads['b2'] = numerical_gradient(loss_W, self,param['b2'])
+
+        return grads
 
     #重みパラメータに対する勾配を誤差逆伝搬法によって求める
     def gradient(self, x, t):
-        # TODO 未実装
+        # forward
+        self.loss(x, t)
+
+        # backward
+        dout = 1
+        dout = self.lastLayer.backward(dout)
+
+        layers = list(self.layers.valuses())
+        layers = reverse()
+        for layer in layers:
+            dout = layer.backward(dout)
+
+        # 設定
+        grads = {}
+        grads['W1'] = self.layers['Affine1'].dW
+        grads['b1'] = self.layers['Affine1'].db
+        grads['W2'] = self.layers['Affine2'].dW
+        grads['b2'] = self.layers['Affine2'].db
+
+        return grads
