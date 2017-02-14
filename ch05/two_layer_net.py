297a4d39aa72a6fabeb285573dcacd4f09b9e84c
shogo watanabe <ne.sheep.5mouths@gmail.com>
2017-02-14 10:01:58 +0900

    記述誤りを修正

--- ch05/two_layer_net.py
+++ ch05/two_layer_net.py
@@ -10,19 +10,19 @@ class TwoLayerNet:
 
     # 初期化処理
     # 引数: 入力層のニューロン数、隠れ層のニューロン数、出力層のニューロン数ｍ重み初期化字のガウス分布のスケール
-    def __init__(self,input_size, hidden_size, output_size, weight_init_std):
+    def __init__(self,input_size, hidden_size, output_size, weight_init_std=0.01):
         # 重みの初期化
-        self.param = {}
-        self.param['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
-        self.param['b1'] = np.zeros(hidden_size)
-        self.param['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
-        self.param['b2'] = np.zeros(output_size)
+        self.params = {}
+        self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
+        self.params['b1'] = np.zeros(hidden_size)
+        self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
+        self.params['b2'] = np.zeros(output_size)
 
         # レイヤの生成
         self.layers = OrderedDict()
-        self.layers['Affin1'] = Affine(self.params['W1'], self.params['b1'])
+        self.layers['Affine1'] = Affine(self.params['W1'], self.params['b1'])
         self.layers['Relu1'] = Relu()
-        self.layers['Affin2'] = Affine(self.params['W2'], self.params['b2'])
+        self.layers['Affine2'] = Affine(self.params['W2'], self.params['b2'])
         self.lastLayer = SoftmaxWithLoss()
 
     #  認識(推論)をおこなう
@@ -30,6 +30,7 @@ class TwoLayerNet:
     def predict(self, x):
         for layer in self.layers.values():
             x = layer.forward(x)
+        return x
 
     # 損失関数の値を求める
     # 引数 x:画像データ、 t:正解ラベル
@@ -51,10 +52,10 @@ class TwoLayerNet:
         loss_W = lambda W: self.loss(x, t)
 
         grads = {}
-        grads['W1'] = numerical_gradient(loss_W, self,param['W1'])
-        grads['b1'] = numerical_gradient(loss_W, self,param['b1'])
-        grads['W2'] = numerical_gradient(loss_W, self,param['W2'])
-        grads['b2'] = numerical_gradient(loss_W, self,param['b2'])
+        grads['W1'] = numerical_gradient(loss_W, self.params['W1'])
+        grads['b1'] = numerical_gradient(loss_W, self.params['b1'])
+        grads['W2'] = numerical_gradient(loss_W, self.params['W2'])
+        grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
 
         return grads
 
@@ -67,8 +68,8 @@ class TwoLayerNet:
         dout = 1
         dout = self.lastLayer.backward(dout)
 
-        layers = list(self.layers.valuses())
-        layers = reverse()
+        layers = list(self.layers.values())
+        layers.reverse()
         for layer in layers:
             dout = layer.backward(dout)
 
