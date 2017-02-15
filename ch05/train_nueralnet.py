e29ab197df142dc699cef446286f7081d7254c7d
shogo watanabe <ne.sheep.5mouths@gmail.com>
2017-02-15 18:03:23 +0900

    コメント追加

--- ch05/train_nueralnet.py
+++ ch05/train_nueralnet.py
@@ -10,6 +10,7 @@ from two_layer_net import TwoLayerNet
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
 network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
 
+# ハイパーパラメータ
 iters_num = 10000
 train_size = x_train.shape[0]
 batch_size = 100
@@ -19,6 +20,7 @@ train_loss_list = []
 train_acc_list = []
 test_acc_list = []
 
+# 1エポックあたりの繰り返し数
 iter_per_epoch = max(train_size / batch_size, 1)
 
 for i in range(iters_num):
